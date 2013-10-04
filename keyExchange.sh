b650b0309d0db6f8de6eafbdbc585a564fbadbe6
Fabio Perfetti <perfabio87@gmail.com>
2013-10-04 14:42:21 +0200

    Controller: fixed version actuator

--- keyExchange.sh
+++ keyExchange.sh
@@ -5,11 +5,16 @@ set +x
 
 source ./env.sh
 
+echo "Generating key pairs on master ${MASTER}"   
+ssh -o StrictHostKeyChecking=no ${USER}@${MASTER} "echo 'y' | ssh-keygen -t rsa -f ~/.ssh/id_rsa -N ''";
+
 echo "Copying key from master ${MASTER} locally"
 scp -o StrictHostKeyChecking=no ${USER}@${MASTER}:.ssh/id_rsa.pub masterPublicKey
 
 for SLAVE in "${SLAVES[@]}"
 do
+	echo "Generating key pairs on slave ${SLAVE}"	
+	ssh -o StrictHostKeyChecking=no ${USER}@${SLAVE} "echo 'y' | ssh-keygen -t rsa -f ~/.ssh/id_rsa -N ''";
 
 	echo "Copying key from slave ${SLAVE} locally"; 
 	scp -o StrictHostKeyChecking=no ${USER}@${SLAVE}:.ssh/id_rsa.pub slavePublicKey; 
@@ -20,8 +25,10 @@ do
 	
 	echo "Adding master [${MASTER}] to authorized_hosts on slave [${SLAVE}]";
 	cat masterPublicKey | ssh -o StrictHostKeyChecking=no root@${SLAVE} cat - ">>" .ssh/authorized_keys;
-  ssh -o StrictHostKeyChecking=no ${USER}@${SLAVE} "chmod 700 ~/.ssh/id_rsa";
+	ssh -o StrictHostKeyChecking=no ${USER}@${SLAVE} "chmod 700 ~/.ssh/id_rsa";
  	
 done
 
+rm slavePublicKey;
+rm masterPublicKey;
 
